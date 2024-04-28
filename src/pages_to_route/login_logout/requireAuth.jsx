0e50dcee4a218d712ea545a9bcf641d867740f17
fxdxlogx <153749212+fxdxlogx@users.noreply.github.com>
2024-04-28 20:13:21 +0545

    Update requireAuth.jsx

--- src/pages_to_route/login_logout/requireAuth.jsx
+++ src/pages_to_route/login_logout/requireAuth.jsx
@@ -1,11 +1,15 @@
+/* to make sure that user is logged in to show profile page */
+
 import { useAuth } from "./authProvider";
+import {useNavigate} from 'react-router-dom'
 
 export function RequireAuth({ children }) {
+  const navigate = useNavigate()
   const auth = useAuth();
 
   if (!auth.login) {
-    return <Navigate to="login"></Navigate>;
+    return navigate('/');
   }
-
+  /*if not logged in return to home page, but if logged in then show profile page. */
   return { children };
 }
